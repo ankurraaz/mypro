5edafc49044a659dc1ec46d8f0638c72d6a7fb04
ankur <ankurraaz111@gmail.com>
2024-05-23 23:22:41 +0530

    oops.py1

---
+++ oops.py
@@ -0,0 +1,15 @@
+class myclass():
+    iname = "nth"
+    location = "hyd"
+
+    def working(self):
+        print("i am working")
+
+    def sleeping(self):
+        print("i am sleeping")
+
+obj1 = myclass()
+print(obj1.iname)
+print(obj1.location)
+obj1.working()
+obj1.sleeping()
